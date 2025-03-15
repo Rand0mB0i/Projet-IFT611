ab1d843bc2b43377af613f196ac5b75a62125469
Nicolas Ouellet <ouen1102@usherbrooke.ca>
2025-03-15 19:29:59 -0400

    Changement de HIDAPI à JoyShockLibrary

--- main.cpp
+++ main.cpp
@@ -2,85 +2,34 @@
 #include <iomanip>
 #include <thread>
 #include <chrono>
-#include <vector>
-#include <hidapi.h>
 #include <windows.h>
 #include <sstream>
+#include <JoyShockLibrary.h>
 
-#define JOYCON_VENDOR  0x057E
-#define JOYCON_PRODUCT 0x2007  // 0x2007 for Left Joy-Con
 using namespace std;
 
 // Global variables to store accelerometer data
-float accelerometer_x = 0.0f;
-float accelerometer_y = 0.0f;
-float accelerometer_z = 0.0f;
+float accel_x = 0.0f;
+float accel_y = 0.0f;
+float accel_z = 0.0f;
 
 HWND hwnd; // Global window handle
-unsigned char raw_data[65] = {};
-constexpr float accel_scale = 0.000244f; // Correct scale for ±8G
 
-// Send command to the device
-void sendCommand(hid_device* handle, const std::vector<unsigned char>& data)
+void UpdateSensorData()
 {
-	hid_write(handle, data.data(), data.size());
-	std::this_thread::sleep_for(std::chrono::milliseconds(50));
-}
+	int handles[1];
+	int count = JslGetConnectedDeviceHandles(handles, 1);
+	int joyConHandle = handles[0];
 
-// Read IMU data from Joy-Con
-void read_imu_data(hid_device* handle)
-{
-	// Enable IMU
-	sendCommand(handle, {0x01, 0x40});
-	std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-	sendCommand(handle, {0x01, 0x40});
-	std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-	// Set IMU sensitivity
-	sendCommand(handle, { 0x41, 0x03, 0x00, 0x00, 0x02 }); // Accel ±8G, Gyro ±2000dps
-	std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-	// Simulate data update
 	while (true)
 	{
-		int bytesRead = hid_read(handle, raw_data, sizeof(raw_data));
+		MOTION_STATE motionState = JslGetMotionState(joyConHandle);
+		accel_x = motionState.accelX;
+		accel_y = motionState.accelY;
+		accel_z = motionState.accelZ;
 
-		if (bytesRead < 5)
-		{
-			std::cerr << "Failed to read IMU data.\n";
-			std::cout << "Nombre de byte lus: " << bytesRead << "\n";
-			this_thread::sleep_for(std::chrono::milliseconds(20));
-			continue;
-		}
-
-		// If we get IMU data (0x30), ignore and keep reading
-		if (raw_data[0] == 0x30)
-		{
-			//std::cout << "Received IMU data..." << std::endl;
-		}
-
-
-		// Read accelerometer data
-		int16_t accel_x_raw = (raw_data[13] << 8) | raw_data[12]; // Little-endian byte order
-		int16_t accel_y_raw = (raw_data[15] << 8) | raw_data[14];
-		int16_t accel_z_raw = raw_data[17] << 8 | raw_data[16];
-
-		float ax = accel_x_raw * accel_scale;
-		float ay = accel_y_raw * accel_scale;
-		float az = accel_z_raw * accel_scale;
-
-		// Update global accelerometer data
-		accelerometer_x = ax;
-		accelerometer_y = ay;
-		accelerometer_z = az;
-
-		cout << "Acceleration:\nX: " << accelerometer_x << "\nY: " << accelerometer_y << "\nZ: " << accelerometer_z << "\n";
-
-		// Notify the window to repaint itself
 		InvalidateRect(hwnd, nullptr, TRUE);
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::milliseconds(15));
 	}
 }
 
@@ -98,25 +47,17 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			PAINTSTRUCT ps;
 			HDC hdc = BeginPaint(hwnd, &ps);
 
+			// Efface l'ancienne valeur en remplissant le fond en blanc
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			HBRUSH brush = CreateSolidBrush(RGB(255, 255, 255));
+			FillRect(hdc, &rect, brush);
+			DeleteObject(brush);
+
 			// Display accelerometer data on the window
-			std::wstringstream ss;
-			/*ss << L"Accelerometer:\n"
-			    << L"X: " << accelerometer_x << L" G\n"
-			    << L"Y: " << accelerometer_y << L" G\n"
-			    << L"Z: " << accelerometer_z << L" G";*/
-
-			// Display the raw byte data in hex
-			for (int i = 0; i < 65; ++i)
-			{
-				ss << std::setw(2) << std::setfill(L'0') << std::hex << raw_data[i] << L" ";
-				if ((i + 1) % 10 == 0)
-				{
-					ss << L"\n"; // New line after every 10 bytes for readability
-				}
-			}
-
-			std::wstring str = ss.str();
-			TextOutW(hdc, 10, 10, str.c_str(), str.length());
+			wchar_t buffer[100];
+			swprintf(buffer, 100, L"X: %.3f\nY: %.3f\nZ: %.3f", accel_x, accel_y, accel_z);
+			DrawTextW(hdc, buffer, -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
 
 			EndPaint(hwnd, &ps);
 			return 0;
@@ -130,23 +71,20 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 // Main function to initialize HID, create window and read IMU data
 int main()
 {
-	// Initialize HIDAPI
-	if (hid_init())
+	int deviceCount = JslConnectDevices();
+	if (deviceCount == 0)
 	{
-		std::cerr << "Failed to initialize HIDAPI\n";
+		std::cerr << L"Aucun peripherique detecte." << std::endl;
 		return -1;
 	}
 
-	// Open the Joy-Con device
-	hid_device* handle = hid_open(JOYCON_VENDOR, JOYCON_PRODUCT, nullptr);
-	if (!handle)
-	{
-		std::cerr << "Failed to open Joy-Con\n";
-		return -1;
-	}
-
-	// Set non-blocking mode
-	hid_set_nonblocking(handle, 1);
+	int handles[1];
+	int count = JslGetConnectedDeviceHandles(handles, 1);
+	int joyConHandle = handles[0];
+	MOTION_STATE motionState = JslGetMotionState(joyConHandle);
+	accel_x = motionState.accelX;
+	accel_y = motionState.accelY;
+	accel_z = motionState.accelZ;
 
 	// Window class registration
 	constexpr wchar_t CLASS_NAME[] = L"AccelerometerWindow";
@@ -178,7 +116,7 @@ int main()
 	UpdateWindow(hwnd);
 
 	// Start a thread to read IMU data
-	std::thread imuThread(read_imu_data, handle);
+	std::thread sensorThread(UpdateSensorData);
 
 	// Message loop
 	MSG msg = {};
@@ -188,9 +126,45 @@ int main()
 		DispatchMessage(&msg);
 	}
 
-	imuThread.join(); // Wait for the IMU thread to finish
+	sensorThread.join();
 
-	hid_close(handle);
-	hid_exit();
 	return 0;
 }
+
+/*
+int main() {
+	int deviceCount = JslConnectDevices();
+	if (deviceCount == 0) {
+		std::cerr << "Aucun peripherique detecte." << std::endl;
+		return -1;
+	}
+
+	int handles[1];
+	int count = JslGetConnectedDeviceHandles(handles, 1);
+	int joyConHandle = handles[0]; // Supposons que le premier périphérique soit le Joy-Con
+	IMU_STATE motionState = JslGetIMUState(joyConHandle);
+	std::cout << R"(Acceleration :)" << std::endl;
+	std::cout << R"(Acc X : )" << motionState.accelX << std::endl;
+	std::cout << R"(Acc Y : )" << motionState.accelY << std::endl;
+	std::cout << R"(Acc Z : )" << motionState.accelZ << std::endl;
+	std::cout << R"(Gyro X : )" << motionState.gyroX << std::endl;
+	std::cout << R"(Gyro Y : )" << motionState.gyroY << std::endl;
+	std::cout << R"(Gyro Z : )" << motionState.gyroZ << std::endl;
+
+	while (true) {
+		motionState = JslGetIMUState(joyConHandle);
+
+		std::cout << R"(Acc X : )" << motionState.accelX << std::endl;
+		std::cout << R"(Acc Y : )" << motionState.accelY << std::endl;
+		std::cout << R"(Acc Z : )" << motionState.accelZ << std::endl;
+		std::cout << R"(Gyro X : )" << motionState.gyroX << std::endl;
+		std::cout << R"(Gyro Y : )" << motionState.gyroY << std::endl;
+		std::cout << R"(Gyro Z : )" << motionState.gyroZ << std::endl;
+
+		// Traitez les données ici
+		std::this_thread::sleep_for(std::chrono::milliseconds(15)); // Ajustez selon vos besoins
+		// Notify the window to repaint itself
+		InvalidateRect(hwnd, nullptr, TRUE);
+	}
+}
+*/
