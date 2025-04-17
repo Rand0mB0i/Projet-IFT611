f194f4c01653d456728b03dc91e5207dbb60cb88
Nicolas Ouellet <ouen1102@usherbrooke.ca>
2025-04-17 15:20:07 -0400

    Mini-jeu de base

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <chrono>
 #include <windows.h>
 #include <sstream>
+#include <random>
+#include <atomic>
 #include <JoyShockLibrary.h>
 
 using namespace std;
@@ -22,7 +24,30 @@ HWND hwnd; // Global window handle
 
 enum Direction { NONE, UP, DOWN, LEFT, RIGHT };
 
-Direction DetectDirection(float x, float y, float threshold = 0.7f)
+Direction currentInstruction = NONE;
+std::chrono::steady_clock::time_point instructionTime;
+std::atomic<bool> waitingForInput = false;
+std::atomic<bool> correctInputReceived = false;
+std::string feedback = "";
+
+Direction GetRandomDirection() {
+	static std::default_random_engine engine(std::random_device{}());
+	std::uniform_int_distribution<int> dist(0, 3);
+	return static_cast<Direction>(dist(engine) + 1);
+}
+
+const wchar_t* DirectionToText(Direction dir) {
+	switch (dir) {
+	case UP: return L"Haut";
+	case DOWN: return L"Bas";
+	case LEFT: return L"Gauche";
+	case RIGHT: return L"Droite";
+	default: return L"Aucun";
+	}
+}
+
+
+Direction DetectDirection(float x, float y, float threshold = 1.0f)
 {
 	if (abs(x) < threshold && abs(y) < threshold)
 		return NONE;
@@ -55,6 +80,17 @@ void UpdateSensorData()
 		accel_x = motionState.accelX - offset_x;
 		accel_y = motionState.accelY - offset_y;
 		accel_z = motionState.accelZ - offset_z;
+
+		Direction detected = DetectDirection(accel_x, accel_y);
+
+		if (waitingForInput && detected == currentInstruction)
+		{
+			auto reactionTime = chrono::duration_cast<chrono::milliseconds>(
+				chrono::steady_clock::now() - instructionTime).count();
+			feedback = "Réaction : " + to_string(reactionTime) + " ms";
+			correctInputReceived = true;
+			waitingForInput = false;
+		}
 
 		InvalidateRect(hwnd, nullptr, TRUE);
 		std::this_thread::sleep_for(std::chrono::milliseconds(15));
@@ -102,6 +138,19 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			RECT dirRect = rect;
 			dirRect.top += 50;
 			DrawTextW(hdc, dirText, -1, &dirRect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+			// Affiche l'instruction actuelle
+			RECT instrRect = rect;
+			instrRect.top += 100;
+			wstring instrText = L"Instruction : ";
+			instrText += DirectionToText(currentInstruction);
+			DrawTextW(hdc, instrText.c_str(), -1, &instrRect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+
+			// Affiche le feedback (temps de réaction)
+			RECT feedRect = rect;
+			feedRect.top += 150;
+			wstring wfeedback(feedback.begin(), feedback.end());
+			DrawTextW(hdc, wfeedback.c_str(), -1, &feedRect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
 
 			EndPaint(hwnd, &ps);
 			return 0;
@@ -167,6 +216,25 @@ int main()
 	// Start a thread to read IMU data
 	thread sensorThread(UpdateSensorData);
 
+	// Start a thread for the game
+	thread gameLoop([] {
+		while (true)
+		{
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+
+			currentInstruction = GetRandomDirection();
+			instructionTime = std::chrono::steady_clock::now();
+			waitingForInput = true;
+			correctInputReceived = false;
+			feedback = "";
+
+			while (!correctInputReceived)
+				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+			std::this_thread::sleep_for(std::chrono::seconds(2));
+		}
+		});
+
 	// Message loop
 	MSG msg = {};
 	while (GetMessage(&msg, nullptr, 0, 0))
@@ -176,6 +244,7 @@ int main()
 	}
 
 	sensorThread.join();
+	gameLoop.join();
 
 	return 0;
 }
